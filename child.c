c7e102b882ab4439bffc2dc7f3c997313ba1632c
Abderrahim Ahaded <abahaded@e2r9p2.1337.ma>
2024-12-02 20:29:14 +0100

    new files and changes

--- child.c
+++ child.c
@@ -2,12 +2,8 @@
 
 void child(pid_t pipefd[2], char **av)
 {
-	char	b[1024];
-
 	close(pipefd[0]);
-	int fd = open("file.txt", O_RDONLY);
-	int end = read(fd, b, sizeof(b));
-	b[end] = '\0';
+	int fd = open(av[1], O_RDONLY);
 	dup2(pipefd[1], 1);
 	char *agv[] = {"cat", av[1], NULL};
 	if (execve("/usr/bin/cat", agv, NULL) == -1)
